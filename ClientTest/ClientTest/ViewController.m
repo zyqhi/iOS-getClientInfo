ff41d6aff145639f5cecb44ea73cf5ddd146ad7d
zyqhi <zyqhi2010@gmail.com>
2019-03-29 16:29:46 +0800

    更新ui

--- ClientTest/ClientTest/ViewController.m
+++ ClientTest/ClientTest/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 #import "BasicViewController.h"
 
-@interface ViewController () 
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSDictionary *infosDict;
 
 @end
 
@@ -17,34 +20,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_tableView];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.rowHeight = 60;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [_tableView registerClass:UITableViewCell.class forCellReuseIdentifier:NSStringFromClass(UITableViewCell.class)];
+    
+    _infosDict = @{
+                   @(BasicInfoTypeHardWare): @"HardWare",
+                   @(BasicInfoTypeBattery): @"Battery",
+                   @(BasicInfoTypeIpAddress): @"IpAddress",
+                   @(BasicInfoTypeCPU): @"CPU",
+                   @(BasicInfoTypeDisk): @"Disk"
+                   };
 }
 
-#pragma mark - Event Response
-- (IBAction)hardWareInfoButtonTapped:(id)sender {
-    [self _pushVCWithType:BasicInfoTypeHardWare sender:sender];
+- (void)viewDidAppear:(BOOL)animated {
+    self.navigationItem.title = nil;
 }
 
-- (IBAction)batteryInfoButtonTapped:(id)sender {
-    [self _pushVCWithType:BasicInfoTypeBattery sender:sender];
-}
+//#pragma mark - Event Response
+//- (IBAction)hardWareInfoButtonTapped:(id)sender {
+//    [self _pushVCWithType:BasicInfoTypeHardWare sender:sender];
+//}
+//
+//- (IBAction)batteryInfoButtonTapped:(id)sender {
+//    [self _pushVCWithType:BasicInfoTypeBattery sender:sender];
+//}
+//
+//- (IBAction)addressInfoButtonTapped:(id)sender {
+//    [self _pushVCWithType:BasicInfoTypeIpAddress sender:sender];
+//}
+//
+//- (IBAction)CPUInfoButtonTapped:(id)sender {
+//    [self _pushVCWithType:BasicInfoTypeCPU sender:sender];
+//}
+//
+//- (IBAction)diskInfoButtonTapped:(id)sender {
+//    [self _pushVCWithType:BasicInfoTypeDisk sender:sender];
+//}
 
-- (IBAction)addressInfoButtonTapped:(id)sender {
-    [self _pushVCWithType:BasicInfoTypeIpAddress sender:sender];
-}
-
-- (IBAction)CPUInfoButtonTapped:(id)sender {
-    [self _pushVCWithType:BasicInfoTypeCPU sender:sender];
-}
-
-- (IBAction)diskInfoButtonTapped:(id)sender {
-    [self _pushVCWithType:BasicInfoTypeDisk sender:sender];
-}
-
-- (void)_pushVCWithType:(BasicInfoType)type sender:(UIButton *)sender {
+- (void)_pushVCWithType:(BasicInfoType)type title:(NSString *)title {
     BasicViewController *basicVC = [[BasicViewController alloc] initWithType:type];
-    basicVC.navigationItem.title = sender.titleLabel.text;
+    basicVC.navigationItem.title = title;
     [self.navigationController pushViewController:basicVC  animated:YES];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _infosDict.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    cell.textLabel.text = _infosDict[@(indexPath.row)];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self _pushVCWithType:indexPath.row title:_infosDict[@(indexPath.row)]];
 }
 
 @end
