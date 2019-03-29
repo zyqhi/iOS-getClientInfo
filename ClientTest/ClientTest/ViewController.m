a3df1fbd51b46b4894a5644312c3ec1d7570efd5
zyqhi <zyqhi2010@gmail.com>
2019-03-29 16:32:09 +0800

    Delete commnet

--- ClientTest/ClientTest/ViewController.m
+++ ClientTest/ClientTest/ViewController.m
@@ -42,27 +42,6 @@
     self.navigationItem.title = nil;
 }
 
-//#pragma mark - Event Response
-//- (IBAction)hardWareInfoButtonTapped:(id)sender {
-//    [self _pushVCWithType:BasicInfoTypeHardWare sender:sender];
-//}
-//
-//- (IBAction)batteryInfoButtonTapped:(id)sender {
-//    [self _pushVCWithType:BasicInfoTypeBattery sender:sender];
-//}
-//
-//- (IBAction)addressInfoButtonTapped:(id)sender {
-//    [self _pushVCWithType:BasicInfoTypeIpAddress sender:sender];
-//}
-//
-//- (IBAction)CPUInfoButtonTapped:(id)sender {
-//    [self _pushVCWithType:BasicInfoTypeCPU sender:sender];
-//}
-//
-//- (IBAction)diskInfoButtonTapped:(id)sender {
-//    [self _pushVCWithType:BasicInfoTypeDisk sender:sender];
-//}
-
 - (void)_pushVCWithType:(BasicInfoType)type title:(NSString *)title {
     BasicViewController *basicVC = [[BasicViewController alloc] initWithType:type];
     basicVC.navigationItem.title = title;
